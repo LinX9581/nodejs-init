acb4127613e110c233d95de5c9212c9ccde61b28
linx9581 <linx9581@gmail.com>
2019-09-08 19:22:06 +0800

    sdfgsdg

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+
+app.use(express.static('public'));  //靜態檔案放置區
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
+
+http.listen(port, host, function() {
+    console.log("Server started.......");
+});
