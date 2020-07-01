f13b5a2248c2fab085ab9ff34aa1942350533104
linx9581 <linx9581@gmail.com>
2020-07-01 16:12:25 +0800

    linebot display name

--- linebot-init.js
+++ linebot-init.js
@@ -8,8 +8,19 @@ bot = linebot({
 
 bot.on('message', function(event) {
 
-    console.log(event);
     var msg = event.message.text;
+    let groupId = event.source.groupId;
+    let userId = event.source.userId;
+    let msgType = event.message.type;
+    let linebotMsgArray = [];
+    bot.getGroupMemberProfile(groupId, userId)
+        .then((profile) => {
+            let displayName = profile.displayName;
+            linebotMsgArray.push("groupId:" + groupId, "userId:" + userId, "msgType: " + msgType, "displayName: " + displayName + ":" + msg)
+            console.log(linebotMsgArray)
+        }).catch((err) => {
+            console.log(err);
+        });
 
     if (msg == "test") {
         event.reply("tesasdfasfasdfasdft").then(function(data) {
@@ -40,3 +51,6 @@ var port = process.env.PORT || 8080;
 http.listen(port, function() {
     console.log('My Line bot App running on 8080');
 });
+
+// 參考
+// https://medium.com/@s1k2y37st/linebot%E7%B3%BB%E5%88%97-%E4%B8%80-%E8%81%8A%E5%A4%A9bot%E7%9A%84helloworld-echo-bot-ec93eb6c734e
