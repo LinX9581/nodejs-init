0c22c148abc98dd8f0d8bbe480dc3de8219da90c
linx9581 <linx9581@gmail.com>
2020-08-05 14:08:34 +0800

    healthy check

---
+++ health-check.js
@@ -0,0 +1,18 @@
+var https = require('https');
+var schedule = require('node-schedule');
+var rule = new schedule.RecurrenceRule();
+rule.second = [0, 30];
+
+schedule.scheduleJob(rule, function() {
+    https.get('https://express.linxnote.club/', function(res) {
+        console.log("statusCode: ", res.statusCode); // <======= Here's the status code
+        // console.log("headers: ", res.headers);
+        if (res.statusCode != 200) {
+            console.log("error code" + res.statusCode)
+        } else {
+            console.log(res.statusCode)
+        }
+    }).on('error', function(e) {
+        console.error(e);
+    });
+})
