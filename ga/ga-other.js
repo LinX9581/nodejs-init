f6eafc8ec6c19363b633091f1bb0d66966453f1c
root <root@nodejs-test.asia-east1-b.c.react-native-oauth-da314.internal>
2020-08-12 16:46:49 +0800

    sort out file

---
+++ ga/ga-other.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+    // res.send('Hey Guys! we have a Gift for You!');
+    res.sendFile(__dirname + '/other.html');
+    console.log("asd")
+});
+
+const host = '0.0.0.0';
+const port = process.env.PORT || 3500;
+
+http.listen(port, host, function() {
+    console.log("Server started.......");
+});
